40d2aa75d9b909b875cf75781bb2e08cf11e04dd
mynameiskatchy <karchietrinh@gmail.com>
2018-03-04 00:42:51 -0500

    intial blocking

--- src/App.js
+++ src/App.js
@@ -2,11 +2,27 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+// Material-UI Components
+// ----------------------
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import RaisedButton from 'material-ui/RaisedButton';
-import TableExampleSimple from './TableExampleSimple';
+import Paper from 'material-ui/Paper';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import AlarmIcon from 'material-ui/svg-icons/action/alarm';
+import ProfileIcon from 'material-ui/svg-icons/social/person';
+import DataIcon from 'material-ui/svg-icons/action/timeline';
 
+import RaisedButton from 'material-ui/RaisedButton';
+
+// Custom React Components
+// -----------------------
+import TableExampleSimple from './TableExampleSimple';
 import usersListPanel from './usersListPanel';
+
+const paperStyle={
+  height: '100%',
+  width: '100%',
+  textAlign: 'center',
+}
 
 class App extends Component {
   render() {
@@ -18,17 +34,32 @@ class App extends Component {
         </header>
         <MuiThemeProvider>
 
-
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 
-        <RaisedButton>Yesss</RaisedButton>
-        <TableExampleSimple></TableExampleSimple>
-        <usersListPanel></usersListPanel>
+        <Paper style={paperStyle}>
+          <Tabs>
+            <Tab 
+              label={"ALARM"}
+              icon={<AlarmIcon/>}>
+              Hello World
+            </Tab>
+            <Tab 
+              label={"HISTORY"}
+              icon={<DataIcon/>}>
+              Hello World 2
+            </Tab>
+            <Tab 
+              label={"PROFILE"}
+              icon={<ProfileIcon/>}>
+              Hello World 3
+            </Tab>
+          </Tabs>
+        </Paper>
         
-        </MuiThemeProvider>
-        </div>
+       </MuiThemeProvider> 
+      </div>
     );
   }
 }
